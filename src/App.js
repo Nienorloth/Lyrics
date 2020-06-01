336e85c61863568827c9970c0fc8c3c375caec2d
Mariana Rascon <nienorloth@gmail.com>
2020-05-31 19:50:40 -0500

    lyrics obtained from API and displayed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
+import Song from './components/Song';
 
 
 function App() {
 
   //define the state
   const [ searchlyrics, saveSearchLyrics ] = useState({});
+  const [ lyrics, saveLyrics ] = useState('');
 
   useEffect(() => {
     if(Object.keys(searchlyrics).length === 0) return;
@@ -15,6 +18,9 @@ function App() {
     const { artist, song } = searchlyrics;
 
    const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+   const resultado = await axios(url);
+   saveLyrics(resultado.data.lyrics);
    }
    consultarApiLetra();  
   }, [searchlyrics])
@@ -24,6 +30,19 @@ function App() {
       <Formulario
         saveSearchLyrics={saveSearchLyrics}
       />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+              <Song 
+                lyrics={lyrics}
+              />
+          </div>
+  
+        </div>
+      </div>
     </Fragment>
   );
 }
